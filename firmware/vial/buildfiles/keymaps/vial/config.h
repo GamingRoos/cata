59a3a6210cbd54dbf8d237e37322a47fcca97264
GamingRoos <landlcopley@gmail.com>
2025-09-24 03:48:07 -0700

    Vial Build files

---
+++ firmware/vial/buildfiles/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3E, 0xCF, 0x7E, 0xA7, 0xF1, 0x14, 0x89, 0x13}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 4 }
